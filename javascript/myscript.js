e396cfa61ad89638c65fdac78cbe7e485ae03499
xtsoulfiretx <alexi.lambrou@gmail.com>
2021-05-17 17:00:38 +0100

    tried to finish side nav

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,11 +1,14 @@
-window.addEventListener('load', function() {
-  $(".sidebar").hide();
-});
+// window.addEventListener('load', function() {
+//   $(".sidebar").hide();
+// });
 
 // Hambuger click effect //
 
   $(".hamburger").click(function(){
     $(".hamburger").toggleClass("is-active");
     $("#main").toggleClass("btn-pressed");
+    $(".sidebar").toggleClass("is-visible");
+    $(".site-wrap").toggleClass("site-wrap-d");
+    $("body").toggleClass("fixed-body");
 });
 
