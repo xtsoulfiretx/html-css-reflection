0b29579012cd8e5e93230a06423d859f718a838e
xtsoulfiretx <alexi.lambrou@gmail.com>
2021-05-17 13:13:49 +0100

    started work on js for menu btn

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,5 +1,11 @@
-  // Hambuger click effect //
+window.addEventListener('load', function() {
+  $(".sidebar").hide();
+});
+
+// Hambuger click effect //
 
   $(".hamburger").click(function(){
     $(".hamburger").toggleClass("is-active");
+    $("#main").toggleClass("btn-pressed");
 });
+
