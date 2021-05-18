7c3ce6c908a507b4e20adc119fddccde8d763902
xtsoulfiretx <alexi.lambrou@gmail.com>
2021-05-18 12:55:57 +0100

    finished work with side nav

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -4,11 +4,13 @@
 
 // Hambuger click effect //
 
-  $(".hamburger").click(function(){
+  function darken() {
     $(".hamburger").toggleClass("is-active");
     $("#main").toggleClass("btn-pressed");
     $(".sidebar").toggleClass("is-visible");
     $(".site-wrap").toggleClass("site-wrap-d");
     $("body").toggleClass("fixed-body");
-});
+};
 
+$(".hamburger").click(darken);
+$(".site-wrap").click(darken);
