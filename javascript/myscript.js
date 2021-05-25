5222517611387999263cfe1db2b3278275f6b358
xtsoulfiretx <alexi.lambrou@gmail.com>
2021-05-25 12:46:24 +0100

    added slick carousel

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,6 +1,3 @@
-// window.addEventListener('load', function() {
-//   $(".sidebar").hide();
-// });
 
 // Hambuger click effect //
 
@@ -19,38 +16,79 @@
 $(".hamburger").click(darken);
 $(".site-wrap").click(darken);
 
+  let lastScrollTop = 0;
 
+window.addEventListener('load', function() {
+   lastScrollTop = window.pageYOffset;
+});
 
 // Sticky Header event listener
 
- let lastScrollTop = 1;
-
  
-  window.addEventListener("scroll", function(){
-
+ window.addEventListener('scroll',function() {
+  clearTimeout($.data(this, 'scrollTimer'));
+  $.data(this, 'scrollTimer', setTimeout(function() {
     const scrolled = window.scrollY; 
+    const headerHeight = $('#head-nav').height();
+    let stickyHeight = scrolled > 300;
+    let st = window.pageYOffset || document.documentElement.scrollTop;
 
-    let stickyHeight = Math.ceil(scrolled) > 300;
-
-    let st = window.pageYOffset || document.documentElement.scrollTop; 
     if (st > lastScrollTop){
-      //  console.log("scrolling down, the header will unstick");
-      $("header").unstick();
-    } else if (st < lastScrollTop && stickyHeight === true) {
-      //  console.log("scrolling Up, the header will stick");
-      $("header").sticky({topSpacing:0});
+      //  Scroll down
+      if ($('#head-nav').css('position') === 'fixed') {
+        $("#head-nav").addClass('scrollDown');
+        // console.log("Scrolled down, will play animation then unstick");
+        $('#head-nav').on('sticky-end', function() { 
+          setTimeout(function(){
+          $("#head-nav").removeClass('scrollDown');
+        },500); 
+        $("#head-nav").unstick();
+      });
+      } else if ($('#head-nav').css('position') === 'relative') {
+        // console.log("Scrolled down but don't need to do anything");
+      }
+    } else if (st === 0 && ($('#head-nav').css('position') === 'fixed')) {
+        // console.log("Scrolled to top, will now unstick")
+        $("#head-nav").unstick();
+    } else if (st <= headerHeight) {
+        //Do Nothing
+        console.log("Doing nothing")
+    } else if (st < lastScrollTop && st > headerHeight) {
+        // Scroll Up
+        if ($('#head-nav').css('position') === 'relative'){
+          $("#head-nav").sticky({topSpacing:0});
+          $('#head-nav').on('sticky-start', function() { 
+            $("#head-nav").addClass('scrollUp');
+          // console.log("Scrolled up, will stick header");
+          setTimeout(function(){
+            $("#head-nav").removeClass('scrollUp');
+          },500); 
+        });
+        }
+        else if ($('head-nav').css('position') === 'fixed') {
+          // console.log("Scrolled up, but Header already present");
+        }
     } else {
       // console.log("nothing will happen");
     }
     lastScrollTop = Math.abs(st); // For Mobile or negative scrolling
- }, false);
 
-
+      console.log("Haven't scrolled in 250ms!");
+  }, 250));
+});
 
 
 
 // potential animation code
 
 //  document.getElementById('head-nav').setAttribute("style","animation-name: scrollUpAnimation");
+     
 
+// Slick Banner Carousel 
 
+$(".content-main").slick({
+  autoplay: true,
+  autoplaySpeed: 4000,
+  arrows: false,
+  dots: true
+});
