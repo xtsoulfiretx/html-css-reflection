61f4745e1d5ff01d28a0224b4652c99bdac43107
xtsoulfiretx <alexi.lambrou@gmail.com>
2021-05-27 12:45:37 +0100

    fixed side nav, slick dots and restarted work on sticky header

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -55,40 +55,41 @@ $(".accept-cookies-btn").click(function (){
     if (st > lastScrollTop){
       //  Scroll down
       if ($('#head-nav').css('position') === 'fixed') {
-        $("#head-nav").addClass('scrollDown');
-        // console.log("Scrolled down, will play animation then unstick");
-        $('#head-nav').on('sticky-end', function() { 
-          setTimeout(function(){
-          $("#head-nav").removeClass('scrollDown');
-        },500); 
-        $("#head-nav").unstick();
-      });
-      } else if ($('#head-nav').css('position') === 'relative') {
-        // console.log("Scrolled down but don't need to do anything");
+        console.log("Scrolled down, will play animation then unstick");
+        $("#head-nav").removeClass("sticky");
       }
-    } else if (st === 0 && ($('#head-nav').css('position') === 'fixed')) {
-        // console.log("Scrolled to top, will now unstick")
-        $("#head-nav").unstick();
-    } else if (st <= headerHeight) {
+      
+      else if ($('#head-nav').css('position') === 'relative') {
+        console.log("Scrolled down but don't need to do anything");
+      }
+    }
+    
+    else if (st === 0 && ($('#head-nav').css('position') === 'fixed')) {
+        console.log("Scrolled to top, will now unstick");
+        $("#head-nav").removeClass("sticky");
+    } 
+    
+    else if (st <= headerHeight) {
         //Do Nothing
         console.log("Doing nothing")
-    } else if (st < lastScrollTop && st > headerHeight) {
+    } 
+    
+    else if (st < lastScrollTop && st > headerHeight) {
+
         // Scroll Up
         if ($('#head-nav').css('position') === 'relative'){
-          $("#head-nav").sticky({topSpacing:0});
-          $('#head-nav').on('sticky-start', function() { 
-            $("#head-nav").addClass('scrollUp');
-          // console.log("Scrolled up, will stick header");
-          setTimeout(function(){
-            $("#head-nav").removeClass('scrollUp');
-          },500); 
-        });
+          console.log("Scrolled up, will stick header");
+          $("#head-nav").addClass("sticky");
         }
+
+
         else if ($('head-nav').css('position') === 'fixed') {
-          // console.log("Scrolled up, but Header already present");
+          console.log("Scrolled up, but Header already present");
         }
-    } else {
-      // console.log("nothing will happen");
+    }
+    
+    else {
+      console.log("nothing will happen");
     }
     lastScrollTop = Math.abs(st); // For Mobile or negative scrolling
 
